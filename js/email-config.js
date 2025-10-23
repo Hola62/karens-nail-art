470aef2d9955d02cdea7b5f1186314c3eac38b0a
holuwahola3 <holuwahola3@gmail.com>
2025-10-23 23:19:02 +0100

    EmailJS: set serviceId to service_1k6kdmg (provided)

--- js/email-config.js
+++ js/email-config.js
@@ -3,8 +3,8 @@
 // Create an account at https://www.emailjs.com/ and set up a service and a template
 
 window.EMAIL_SETTINGS = {
-  // e.g., 'service_xxxxxx'
-  serviceId: 'REPLACE_WITH_YOUR_SERVICE_ID',
+  // Set from your message: service_1k6kdmg
+  serviceId: 'service_1k6kdmg',
   // e.g., 'template_yyyyyy'
   templateId: 'REPLACE_WITH_YOUR_TEMPLATE_ID',
   // e.g., 'a1b2C3D4E5F6G7H8I'
